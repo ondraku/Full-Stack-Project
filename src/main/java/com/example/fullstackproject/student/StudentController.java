de10ccdd7d46076b5574523f88a2de791a24d35e
ondraku <ondra.kukla@gmail.com>
2022-03-21 13:32:59 +0100

    Student model and controller added

---
+++ src/main/java/com/example/fullstackproject/student/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.fullstackproject.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        List<Student> students = List.of(
+                new Student(
+                    1L,
+                    "Jane",
+                    "jane@email.com",
+                    Gender.FEMALE),
+                new Student(
+                    2L,
+                    "Alex",
+                    "alex@email.com",
+                    Gender.MALE)
+        );
+        return students;
+    }
+}
